c607ae8b0a821bf1b67327fa5e745da5d14e341c
antonleybov <leybov.anton@gmail.com>
2024-02-15 13:14:31 +0200

    chore(tests): setup tests;

---
+++ src/lib.spec.ts
@@ -0,0 +1,8 @@
+import { StackModals } from './lib'
+
+const ServiceInstance = new StackModals()
+
+test('StackModals instance is initialized', () => {
+    expect(ServiceInstance).toBeDefined()
+})
+
