ccccea4dee6b8325a80a908c504d7bf6c42d7d58
antonleybov <leybov.anton@gmail.com>
2024-02-15 14:34:57 +0200

    chore(tests): add basic testing flow;

--- src/lib.spec.ts
+++ src/lib.spec.ts
@@ -1,8 +1,38 @@
-import { StackModals } from './lib'
+import {StackModals} from './lib'
+import {createModal} from "./utils";
 
-const ServiceInstance = new StackModals()
+const name = 'MODAL_NAME_1' as const
+type Props = {
+    title: string;
+    subTitle?: string
+}
 
-test('StackModals instance is initialized', () => {
-    expect(ServiceInstance).toBeDefined()
+describe(StackModals, () => {
+    const ServiceInstance = new StackModals({
+        ...createModal<typeof name, Props>(name)
+    })
+
+    beforeEach(() => {
+        ServiceInstance.stack.clear()
+    })
+
+    test('instance is initialized', () => {
+        expect(ServiceInstance).toBeInstanceOf(StackModals);
+    })
+
+    test('Modal added', () => {
+        expect(ServiceInstance.stack.size).toBe(0);
+
+        const modalKey = ServiceInstance.addModal("MODAL_NAME_1", { title: 'Title', subTitle: 'subtitle' })
+
+        expect(ServiceInstance.stack.has(modalKey)).toBeDefined()
+    })
+
+
+    test('Modal key is uniq', () => {
+        const modalKey1 = ServiceInstance.addModal("MODAL_NAME_1", { title: 'Title', subTitle: 'subtitle' })
+        const modalKey2 = ServiceInstance.addModal("MODAL_NAME_1", { title: 'Title', subTitle: 'subtitle' })
+
+        expect(modalKey1).not.toBe(modalKey2)
+    })
 })
-
